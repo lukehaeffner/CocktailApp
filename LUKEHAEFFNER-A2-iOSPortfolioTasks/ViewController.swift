7fa52eebc559490f45edd469a8149e1e24311e5e
Luke Haeffner <lukehaeffner@gmail.com>
2020-04-08 15:35:54 +1000

    Initial Commit

---
+++ LUKEHAEFFNER-A2-iOSPortfolioTasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LUKEHAEFFNER-A2-iOSPortfolioTasks
+//
+//  Created by Luke Haeffner on 8/4/20.
+//  Copyright © 2020 Luke Haeffner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
